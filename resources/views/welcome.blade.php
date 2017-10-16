ac02b4762b3be68a476f1b18cf062af02787baf1
Detharo <o.moena24@gmail.com>
2017-10-16 00:56:59 -0300

    titulo a victoria

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Victoria
                 </div>
 
                 <div class="links">
