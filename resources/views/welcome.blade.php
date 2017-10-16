962a75c8e54dad8bc5da568b7343c616b5860d36
Detharo <o.moena24@gmail.com>
2017-10-16 01:40:14 -0300

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Victoria
+                    Victoria 2
                 </div>
 
                 <div class="links">
